12299d979d4c1eec26e20ede4d695990c5126d2d
Jihun Kim <kim1jeahun@naver.com>
2022-04-19 16:42:01 +0900

    미션 2 제출
    모델 만들기

---
+++ support/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+
+from .models import Faq, Inquiry, Answer
+
+@admin.register(Faq)
+class FaqModelAdmin(admin.ModelAdmin):
+    list_display = ('content', 'category', 'writer', 'answer', 'created_at', 'last_modifier', 'modifed_at')
+    list_filter = ('created_at',)
+    
+    search_fields = ('id', 'writer__username')
+    search_help_text = '계시판 번호, 작성자 검색이 가능합니다'
+
+
+class AnswerInline(admin.TabularInline):
+    model = Answer
+    extra = 0
+
+@admin.register(Inquiry)
+class InquiryModelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'category', 'writer', 'e_mail', 'phone_number', 'content', 'image', 'created_at', 'modifed_at')
+    list_filter = ('created_at',)
+    
+    search_fields = ('id', 'writer__username')
+    search_help_text = '계시판 번호, 작성자 검색이 가능합니다'
+
+    inlines = [AnswerInline]
+
+
